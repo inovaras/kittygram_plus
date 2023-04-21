00363edfa6850adde1361fed3529dfe3bcdb5226
Альбина Гильмутдинова <inovaras@mail.ru>
2023-04-21 18:18:45 +0500

    refactor

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,16 +1,47 @@
+import datetime as dt
+
+from .models import Cat, Owner, Achievement, AchievementCat
 from rest_framework import serializers
 
-from .models import Cat, Owner
+
+class AchievementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Achievement
+        fields = ('id', 'name')
 
 
 class CatSerializer(serializers.ModelSerializer):
+    # owner = serializers.PrimaryKeyRelatedField(read_only=True)
+    achievements = AchievementSerializer(many=True, required=False)
+    age = serializers.SerializerMethodField()
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements', 'age')
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
+
+    def create(self, validated_data):
+        if 'achievements' not in self.initial_data:
+            cat = Cat.objects.create(**validated_data)
+            return cat
+
+        achievements = validated_data.pop('achievements')
+        cat = Cat.objects.create(**validated_data)
+        for achievement in achievements:
+            current_achievement, status = Achievement.objects.get_or_create(**achievement)
+
+            AchievementCat.objects.create(
+                achievement=current_achievement, cat=cat
+            )
+        return cat
 
 
 class OwnerSerializer(serializers.ModelSerializer):
+    cats = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
     class Meta:
         model = Owner
         fields = ('first_name', 'last_name', 'cats')
+
--- cats/views.py
+++ cats/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 
-from .models import Cat, Owner
-from .serializers import CatSerializer, OwnerSerializer
+from .models import Cat, Owner, Achievement
+from .serializers import CatSerializer, OwnerSerializer, AchievementSerializer
 
 
 class CatViewSet(viewsets.ModelViewSet):
@@ -12,3 +12,8 @@ class CatViewSet(viewsets.ModelViewSet):
 class OwnerViewSet(viewsets.ModelViewSet):
     queryset = Owner.objects.all()
     serializer_class = OwnerSerializer
+
+
+class AchievementViewSet(viewsets.ModelViewSet):
+    queryset = Achievement.objects.all()
+    serializer_class = AchievementSerializer
